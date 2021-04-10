583baab90114e546b71e6c87dfeb600d48e9c959
heitordeve <heitor.developer@outlook.com>
2021-04-10 13:55:02 -0300

    Projeto de estudo sobre ListView com Android + Java

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+        "Angra dos Reis", "Caldas Novas", "Campos do Jordão", "Costa do Sauípe",
+        "Ilhéus", "Porto Seguro", "Tiradentes", "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos Aires", "Budapeste", "Cancún", "Aruba", "Campinas"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        listLocais.setAdapter( adaptador );
+
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+                String valorSelecionado = listLocais.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), valorSelecionado, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
